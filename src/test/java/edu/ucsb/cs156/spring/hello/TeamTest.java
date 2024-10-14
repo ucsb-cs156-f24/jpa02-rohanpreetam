9dc9a2d2df57c717b4d38679cd59d6f98bb20ef3
rohanpreetam <rohanpreetam@ucsb.edu>
2024-10-14 15:47:05 -0700

    Passed all tests

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,10 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.beans.Transient;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,8 +23,62 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+
+    public void hashCode_returns_correct_hashcode(){
+        Team t = new Team();
+        int result = t.hashCode();
+        int expectedResult = 1;
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+
+    public void equals_returns_correct_boolean() {
+        Team t = new Team();
+        t.setName("Rohan");
+        t.addMember("Shriaan");
+        t.addMember("Jayden");
+        t.addMember("Claire");
+        t.addMember("Tiffany");
+        t.addMember("Kelly");
+
+        Team t2 = new Team();
+        t2.setName("Otherteam");
+        Team t3 = new Team();
+        t3.setName("Team 3");
+        t3.addMember("Brian");
+        t3.addMember("Kevin");
+        assertTrue(t.equals(t));
+        assertTrue(t2.equals(t2));
+        assertFalse(t.equals(t2));
+
+        
+        // t.setMembers(("Shriaan", "Jayden", "Claire", "Tiffany", "Kelly");
+        
+        assertFalse(t.getName().equals(t3.getName()) && t.getMembers().equals(t3.getMembers()));
+
+    }
+
+    @Test
+    public void equals_test_null() {
+        assert(!team.equals(null));
+    }
+
+    
+
+
+
+
    
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+
+
+   
+  
 
 }
